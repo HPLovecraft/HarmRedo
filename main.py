ea2b793b79e3a63874848c8ee48d3c01a0265f6c
HPLovecraft <65413894+HPLovecraft@users.noreply.github.com>
2022-10-03 10:13:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import PRAW as p
+
+HarmRepoBot = p.Reddit(client_id ='my client id',
+					client_secret ='my client secret',
+					user_agent ='my user agent',
+					username ='my username',
+					password ='my password')
 
