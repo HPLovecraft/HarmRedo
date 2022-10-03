4203dc30b7bb41412b66501dfd95c757462980f6
HPLovecraft <65413894+HPLovecraft@users.noreply.github.com>
2022-10-03 10:58:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-import PRAW as p
+import praw
 
-HarmRepoBot = p.Reddit(client_id ='my client id',
-					client_secret ='my client secret',
-					user_agent ='my user agent',
-					username ='my username',
-					password ='my password')
+HarmRedoBot = praw.Reddit(username = "user",
+                password = "pass",
+                client_id = "id",
+                client_secret = "scret",
+                user_agent = "HarmRedoBot by Flood")
 
+messages = HarmRedoBot.inbox.stream() # makes inbox var
+for message in messages: # scan inbox
+  try:
+    if message in HarmRedoBot.inbox.mentions() and message in HarmRedoBot.inbox.unread(): # If mention | read and unread
+        message.reply("hello") # reply with
+        message.mark_read() # mark as read
+	
+  except praw.exceptions.APIException: # If rate limit is reached 
+    print("ERROR | API request limit reached...")
